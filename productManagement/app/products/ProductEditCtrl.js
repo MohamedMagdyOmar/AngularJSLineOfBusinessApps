501097ef759e23b9f4dd4865bc033ecdedd0a016
MohamedMagdyOmar <mohamed.magdy.eg@schneider-electric.com>
2017-10-12 11:37:20 +0200

    6.6- Using a Date Picker

--- productManagement/app/products/ProductEditCtrl.js
+++ productManagement/app/products/ProductEditCtrl.js
@@ -31,7 +31,10 @@
 
         vm.submit = function()
         {
-            vm.product.$save();
+            // vm.product.$save();
+            vm.product.$save(function(data){
+                toastr.success("Save Successful");
+            });
         }
 
         vm.cancel = function()
@@ -39,6 +42,25 @@
             // to navigate to different state
             // since we are using $ state, so we need to inject it in the controller
             $state.go("productList")
+        }
+
+        vm.addTags = function(tags)
+        {
+            if(tags)
+            {
+                var array = tags.split(',');
+                vm.product.tags = vm.product.tags ? vm.product.tags.concat(array):array;
+                vm.newTags = "";
+            }
+            else
+            {
+                alert("Please enter one or more tags separated by commas");
+            }
+        }
+
+        vm.removeTag = function(idx)
+        {
+            vm.product.tags.splice(idx, 1);
         }
     }
 
