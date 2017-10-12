f200152a603cafa716fff67237e8f336273a50dd
MohamedMagdyOmar <mohamed.magdy.eg@schneider-electric.com>
2017-10-12 09:58:52 +0200

    6.7- Submitting the Form

--- productManagement/app/products/ProductEditCtrl.js
+++ productManagement/app/products/ProductEditCtrl.js
@@ -3,13 +3,14 @@
     angular
         .module("productManagement")
         .controller(
-        "ProductEditCtrl", ["product", ProductEditCtrl]);
+        "ProductEditCtrl", ["product","$state", ProductEditCtrl]);
     // the second parameter of the controller is array, the first element of the array is the parametr for the function
     // the second element is a reference to the controller function itself
-    function ProductEditCtrl(product)
+    function ProductEditCtrl(product, $state)
     {
         var vm = this;
         vm.product = product;
+
         if(vm.product && vm.product.productId)
         {
             vm.title = "Edit: " + vm.product.productName;
@@ -26,6 +27,18 @@
             // to prevent event from propagating
 
             vm.opened = !vm.opened;
+        }
+
+        vm.submit = function()
+        {
+            vm.product.$save();
+        }
+
+        vm.cancel = function()
+        {
+            // to navigate to different state
+            // since we are using $ state, so we need to inject it in the controller
+            $state.go("productList")
         }
     }
 
